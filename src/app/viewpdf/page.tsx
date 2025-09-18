daaf261276774a559cf87c9edbdc605bf56e6496
Pratik Yesane <pratiky2000@gmail.com>
2025-09-18 17:29:14 +0530

    Refactor Page component for improved layout and consistency in styling

--- src/app/viewpdf/page.tsx
+++ src/app/viewpdf/page.tsx
@@ -16,24 +16,24 @@ const Page = () => {
                 className="absolute top-4 left-4 cursor-pointer hover:text-stone-700 p-1"
                 onClick={() => router.back()}
             >
-                <ChevronLeft size={36} strokeWidth={2} color='black'/>
+                <ChevronLeft size={36} strokeWidth={2} color="black" />
             </div>
-            <div className="w-[80vw] md:w-[70vw] lg:w-[50vw] flex flex-col gap-4 border px-6 py-10 shadow-lg mt-36">
+            <div className="w-[90vw] sm:w-[80vw] md:w-[70vw] lg:w-[50vw] flex flex-col gap-4 border px-6 py-10 shadow-lg mt-36">
                 {Object.entries(formData).map(([key, value]) => (
                     <div
                         key={key}
                         className="grid grid-cols-[150px_1fr] gap-4 items-baseline"
                     >
-                        <h2 className="text-black text-base font-semibold capitalize">
+                        <h2 className="text-black text-sm sm:text-base font-semibold capitalize">
                             {key.replace(/([A-Z])/g, ' $1')}:
                         </h2>
-                        <p className="text-sm text-stone-600 font-medium w-[80%]">
+                        <p className="text-xs sm:text-sm text-stone-600 font-medium w-[80%]">
                             {value ? value : '-'}
                         </p>
                     </div>
                 ))}
             </div>
-            <div className="w-[80vw] md:w-[70vw] lg:w-[50vw]">
+            <div className="w-[90vw] sm:w-[80vw] md:w-[70vw] lg:w-[50vw]">
                 <DownloadPdf formData={formData} />
             </div>
         </div>
